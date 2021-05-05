a27aaddad837fa3be99e2ea014226a8bab66ea39
Asier <blazgar@gmail.com>
2021-05-05 12:19:35 +0200

    First commit
    Probando la conexion con la bd del server

---
+++ conexion.php
@@ -0,0 +1,47 @@
+<?php
+
+
+
+function ConnectDataBase()		{
+
+$local=0;		
+
+
+	if($local==0){		
+
+			if (!($conexion=mysqli_connect("localhost","root","")))
+			{
+			echo "There is an error connecting the server.";
+			exit();
+			}
+			if (!mysqli_select_db($conexion,"erlete"))
+			{
+			echo "There is an error selecting the DB.";
+			exit();
+            }
+            
+			return $conexion;
+		}
+		else {
+			if (!($conexion=mysqli_connect("204.204.3.1","AOAZ","dam1")))
+			{
+			echo "There is an error connecting the server.";
+			exit();
+			}
+			if (!mysqli_select_db($conexion,"erlete"))
+			{
+			echo "There is an error selecting the DB.";
+			exit();
+            }
+            
+			return $conexion;
+		}
+
+		}
+
+
+	
+
+
+
+?>
---
+++ registrarse.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registro</title>
+</head>
+<body>
+
+
+<h1>Registro</h1>
+
+
+</body>
+</html>
