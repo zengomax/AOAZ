c3faced41a1f421c7c852601b2d209f8001e2b8e
Asier <blazgar@gmail.com>
2021-05-05 13:10:14 +0200

    prueba

--- registrarse.php
+++ registrarse.php
@@ -2,12 +2,36 @@
 <html>
 <head>
 	<title>Registro</title>
+ <link rel="stylesheet" href="lib/css/bootstrap.min.css">
+
 </head>
 <body>
 
 
-<h1>Registro</h1>
+<div class="container">
+  <h1 class="display-4">Registro</h1>
 
 
+<form>
+  <div class="form-group">
+    <label >Email address
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></label>
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+</div>
+
+
+</div>
 </body>
 </html>
