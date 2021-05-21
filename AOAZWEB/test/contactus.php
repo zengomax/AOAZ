2f131809d2d26b16a91e5761e4e6a2b89644574b
moneoasier <moneo.asier@uni.eus>
2021-05-21 11:07:05 +0200

    web update

---
+++ AOAZWEB/test/contactus.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CONTACT US</title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="shortcut icon" href="img/ico.png">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		
+		<!-- Popper JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+		
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+		
+
+		<style type="text/css" media="screen">
+
+			 body{
+				  margin: auto;
+				  padding: 0;
+				  font-family: sans-serif;
+				  background: #FFB133;
+				}
+				.navbar-customclass .navbar-nav .nav-link{
+					  color:#ff8c00;
+					}
+
+			#close:hover{
+
+				background-color: #DE2424;
+				border-radius:5px;
+			}
+
+			#profile:hover{
+				background-color: #1A9516;
+			}
+
+
+			
+
+			#titulo {
+				width: 100%;
+				position: absolute;
+				padding: 0px;
+				margin: 0px auto;
+				text-align: center;
+				font-size: 27px;
+				color: rgba(255, 255, 255, 1);
+				font-family: 'Open Sans', sans-serif;
+				z-index: 9999;
+				text-shadow: 0px 1px 2px rgba(0, 0, 0, 0.33), -1px 0px 2px rgba(255, 255, 255, 0);
+			}
+			footer {
+  				text-align: center;
+ 				padding: 3px;
+  				background-color: #828282;
+  				color: white;
+			}
+			.fa {
+			  padding: 10px;
+			  font-size: 30px;
+			  margin: auto;
+			  text-decoration: none;
+			  margin: 5px 2px;
+			  border-radius: 80%;
+			}
+
+			.fa:hover{
+			    opacity: 0.7;
+			}
+
+			.fa-facebook {
+			  background: #3B5998;
+			  color: white;
+			}
+			.fa-twitter {
+			  background: #55ACEE;
+			  color: white;
+			}
+
+			.fa-youtube {
+			  background: #bb0000;
+			  color: white;
+			}
+
+			.fa-instagram {
+			  background: #125688;
+			  color: white;
+			}
+
+		</style>
+</head>
+<body>
+	<!-- Starts nav bar-->
+	<div class="bs-example">
+		<img src="img/banner2.png" width=100% height=20% ></img>
+	<nav class="navbar navbar-expand-md navbar-customclass bg-dark">
+				
+				<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div id="navbarCollapse" class="collapse navbar-collapse">
+					<ul class="nav navbar-nav ">
+						<li class="nav-item">
+							<a href="index.php" class="nav-link">Home</a>
+							
+						</li>
+						<li><a href="contactus.php" class="nav-link">Contact Us</a></li>
+						
+					</ul>
+					
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
+						<li class="nav-item"><a href="registrarse.php" class="nav-link">Register</a></li>
+						
+					</ul>
+					
+					
+				</div>
+			</nav>
+	</div>
+
+
+<!-- Ends nav bar-->
+
+<!-- Starts carrousel-->
+<div class="container">
+
+
+
+
+
+
+<!-- Ends carrousel-->
+
+
+<!--starts text-->
+<div style="background-color: #ffd966;" >
+	
+	<h1><b>Contact Us</b></h1>
+
+</div>
+<div style="background-color: #ffd966;">
+		<p>
+Have a question? Fill out the Contact Form, and we will respond to your inquiry within 1-3 business days. To place an order over the phone using a credit card payment, please call us at (+34)654389234.</p>
+	
+		
+
+	</div>
+	<div style="background-color: #ffd966;" >
+		<h1>Where we are?</h1>
+	</div>
+	<div style="background-color: #ffd966;">
+	<p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5823.536138572394!2d-2.5907402754433626!3d43.130398196489956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4fd2dc119be3f7%3A0xd7f06f42b374b0fe!2s48291%20Axpe%20Achondo%2C%20Vizcaya!5e0!3m2!1ses!2ses!4v1621583057157!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></p>
+	
+
+	</div>
+	
+
+
+
+
+</div>
+
+
+
+<!--Footer-->
+	<footer>
+	<div class="container-fluid">
+		<div class="row">
+
+		<div class="col-4" style="float:left;margin:auto;">
+			<p>Achondo, Jauregi Street 28<br>
+			Email:aoazdebelopers@gmail.com<br>
+			Telf:654389234</p>			
+		</div>
+		<div class="col-4" style="float:center;margin:auto;">
+			<h1 style="font-size: 25px">Hours</h1>
+			<p>Monday-Friday: 9:00AM-8:00PM<br>
+			Saturday:10:00AM-5:00PM<br>
+			Sunday:11:00AM-6:00PM</p>
+			<p>Holiday hours may vary.</p>
+		</div>
+		<div class="col-4" style="margin:auto;">
+			<h1 style="font-size: 25px">Follow Us</h1>
+			<a href="#" class="fa fa-facebook"></a>
+			<a href="#" class="fa fa-twitter"></a>
+			<a href="#" class="fa fa-youtube"></a>
+			<a href="#" class="fa fa-instagram"></a>
+			<p>@Desing by:AOAZ</p>
+		</div>
+	</div>
+</div>
+</footer>	
+
+
+    		
+
+
+</body>
+</html>
